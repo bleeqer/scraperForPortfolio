c7c2fa907cf78f3a436a36c65dc6f18004d89062
bleeq <bleeqtheproducer@gmail.com>
2021-12-16 21:17:54 +0900

    이미지 파일 로컬 저장 기능

--- src/main/java/scrapers/scrapper.java
+++ src/main/java/scrapers/scrapper.java
@@ -5,13 +5,21 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+
+
 
 
 public class scrapper {
 
     private static String baseUrl = "https://modernif.co.kr";
     private static final String searchUrl = "https://modernif.co.kr/product/list.html?cate_no=24&";
+
+    private static String IMAGE_DESTINATION_FOLDER = "C:/images";
 
     public static void main(String [] args) {
 
@@ -56,12 +64,44 @@ public class scrapper {
                         String productPrice = detailDocument.select("#detail_wrap div div.detailArea div div.product_info ul li.price").html();
                         String productImage = detailDocument.select("#detail_wrap div div.xans-element-.xans-product.xans-product-image.imgArea div img").attr("src").substring(detailDocument.select("#detail_wrap div div.xans-element-.xans-product.xans-product-image.imgArea div img").attr("src").lastIndexOf("/") + 1);
 
+                        downloadImage(detailDocument.select("#detail_wrap div div.xans-element-.xans-product.xans-product-image.imgArea div img").attr("src"));
+
                         System.out.println(productName + " " + productPrice + " " + productImage);
 
 
                     }
                 }
             }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // image download
+    private static void downloadImage(String strImageURL) {
+
+        // get the file name
+        String strImageName = strImageURL.substring(strImageURL.lastIndexOf("/") + 1);
+
+        System.out.println("Saving: " + strImageName + ", from: + strImageURL");
+
+        try {
+
+            URL urlImage = new URL(strImageURL);
+            InputStream in = urlImage.openStream();
+
+            byte[] buffer = new byte[4096];
+            int n = -1;
+
+            OutputStream os = new FileOutputStream(IMAGE_DESTINATION_FOLDER + "/" + strImageName);
+
+            while ((n = in.read(buffer)) != -1) {
+                os.write(buffer, 0, n);
+            }
+
+            os.close();
+
+            System.out.println("IMAGE SAVED");
         } catch (IOException e) {
             e.printStackTrace();
         }
